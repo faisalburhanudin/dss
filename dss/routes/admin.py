4d48ac7f678334bb92bac06bdbc3eba25296e730
faisalburhanudin <gogildrive@gmail.com>
2016-05-24 19:14:48 +0700

    form add user

--- dss/routes/admin.py
+++ dss/routes/admin.py
@@ -1,16 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint(__name__, 'admin')
 
 
 @bp.route("/admin")
 def admin_home():
-    return "Home admin"
+    return render_template("admin/index.html")
+
+
+@bp.route("/admin/user")
+def user_home():
+    return "user list"
 
 
 @bp.route("/admin/user/add")
 def user_add():
-    return "user add"
+    return render_template("admin/user_add.html")
 
 
 @bp.route("/admin/user/add", methods=["POST"])
@@ -22,3 +27,8 @@ def user_add_post():
 @bp.route("/admin/user/update")
 def user_update():
     return "user update"
+
+
+@bp.route("/admin/user/update", methods=["POST"])
+def user_update_action():
+    return  "action user update"
--- tests/tests_admin.py
+++ tests/tests_admin.py
@@ -6,9 +6,9 @@ class AdminTestCase(ServerTest):
     def test_home_admin(self):
         response = self.app.get("/admin")
 
-        self.assertEqual("Home admin", response.data.decode("utf-8"))
+        self.assertIn("Administrator page", response.data.decode("utf-8"))
 
     def test_admin_add_user_get(self):
         response = self.app.get("/admin/user/add")
 
-        self.assertEqual("Add user", response.data.decode("utf-8"))
+        self.assertIn("Add user", response.data.decode("utf-8"))

ad853976821c78fbae6ea584973a32eaa5f57b82
faisalburhanudin <gogildrive@gmail.com>
2016-05-26 19:39:43 +0700

    action update user administrator

--- dss/routes/admin.py
+++ dss/routes/admin.py
@@ -45,6 +45,7 @@ def user_add_post():
 def user_update(user_id):
     """Form update user"""
     administrator = Administrator.query.filter_by(id=user_id).first()
+
     return render_template(
         "admin/user_update.html",
         admin_id=administrator.id,
@@ -55,5 +56,18 @@ def user_update(user_id):
 
 
 @bp.route("/admin/user/update/<int:user_id>", methods=["POST"])
-def user_update_action():
-    return  "action user update"
+def user_update_action(user_id):
+    """action update user"""
+    username = request.form.get("username")
+    status = request.form.get("status")
+    password = request.form.get("password")
+
+    administrator = Administrator.query.filter_by(id=user_id).first()
+    administrator.username = username
+    administrator.status = status
+    administrator.password = password
+
+    db.session.add(administrator)
+    db.session.commit()
+
+    return  "User berhasil diupdate"
--- tests/tests_admin.py
+++ tests/tests_admin.py
@@ -24,7 +24,7 @@ class AdminTestCase(ServerTest):
         self.assertIn('name="status"', response)
         self.assertIn('name="password"', response)
 
-    def test_addmin_add_user_post(self):
+    def test_admin_add_user_post(self):
         response = self.app.post("/admin/user/add", data={
             "username": "faisal",
             "status": "2",
@@ -50,3 +50,22 @@ class AdminTestCase(ServerTest):
         self.assertIn('name="username" value="{}"'.format(user.username), response)
         self.assertIn('value="1" selected', response)
         self.assertIn('name="password" value="{}"'.format(user.password), response)
+
+    def test_admin_update_user_post(self):
+        # add one user
+        user = Administrator("foo", 1, "bar")
+        db.session.add(user)
+        db.session.commit()
+
+        response = self.app.post("/admin/user/update/{}".format(user.id), data={
+            "username": "nama baru cie",
+            "status": 2,
+            "password": "passwordbarujuga"
+        }).data.decode("utf-8")
+
+        self.assertIn("User berhasil diupdate", response)
+
+        user = Administrator.query.filter_by(id=1).first()
+        self.assertEqual("nama baru cie", user.username)
+        self.assertEqual(2, user.status)
+        self.assertEqual("passwordbarujuga", user.password)
